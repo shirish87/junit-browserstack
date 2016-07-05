899b793a6b6d52bed481a3507a7b692614bfd63a
Shirish Kamath <shirishk.87@gmail.com>
2016-07-05 20:45:26 +0530

    Refactor * Support for mvn test -P <profile> or mvn test -D<profile>=true * Minor code refactor * Added missing Parallelized class

--- src/test/java/com/browserstack/BrowserStackJUnitTest.java
+++ src/test/java/com/browserstack/BrowserStackJUnitTest.java
@@ -1,92 +1,94 @@
 package com.browserstack;
+
 import com.browserstack.local.Local;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.Iterator;
-import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileReader;
+import java.net.URL;
+import java.util.*;
 
 @RunWith(Parallelized.class)
+@Ignore
 public class BrowserStackJUnitTest {
     public WebDriver driver;
-    private Local l;
+    private Local browserStackLocal;
 
     private static JSONObject config;
 
     @Parameter(value = 0)
-    public int task_id;
+    public int taskId;
 
     @Parameters
     public static Iterable<? extends Object> data() throws Exception {
         JSONParser parser = new JSONParser();
         config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + System.getProperty("config")));
-        int envs = ((JSONArray)config.get("environments")).size();
+        int envs = ((JSONArray) config.get("environments")).size();
 
-        ArrayList<Integer> task_ids = new ArrayList<Integer>();
-        for(int i=0; i<envs; i++) task_ids.add(i);
+        List<Integer> taskIds = new ArrayList<Integer>();
+        for (int i = 0; i < envs; i++) {
+            taskIds.add(i);
+        }
 
-        return task_ids;
+        return taskIds;
     }
 
+    @SuppressWarnings("unchecked")
     @Before
     public void setUp() throws Exception {
         JSONArray envs = (JSONArray) config.get("environments");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        envs.get(taskId);
 
-        HashMap<String, String> envCapabilities = (HashMap<String, String>) envs.get(task_id);
-        Iterator it = envCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            HashMap.Entry pair = (HashMap.Entry)it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        Map<String, Object> commonCapabilities = (HashMap<String, Object>) config.get("capabilities");
+        for (Map.Entry<String, Object> capability : commonCapabilities.entrySet()) {
+            capabilities.setCapability(capability.getKey(), capability.getValue().toString());
         }
-        
-        HashMap<String, String> commonCapabilities = (HashMap<String, String>) config.get("capabilities");
-        it = commonCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            HashMap.Entry pair = (HashMap.Entry)it.next();
-            if(capabilities.getCapability(pair.getKey().toString()) == null)
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+
+        Map<String, Object> envCapabilities = (HashMap<String, Object>) envs.get(taskId);
+        for (Map.Entry<String, Object> capability : envCapabilities.entrySet()) {
+            capabilities.setCapability(capability.getKey(), capability.getValue().toString());
         }
 
         String username = System.getenv("BROWSERSTACK_USERNAME");
-        if(username == null) username = (String) config.get("user");
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if(accessKey == null) accessKey = (String) config.get("key");
-
-        if(capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true"){
-            l = new Local();
-            HashMap<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            l.start(options);
+        if (username == null) {
+            username = (String) config.get("user");
         }
 
-        driver = new RemoteWebDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if (accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+
+        if ("true".equals(capabilities.getCapability("browserstack.local"))) {
+            browserStackLocal = new Local();
+            Map<String, String> options = new HashMap<String, String>();
+            options.put("key", accessKey);
+            browserStackLocal.start(options);
+        }
+
+        driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
     }
 
     @After
     public void tearDown() throws Exception {
-        driver.quit();
-        if(l != null) l.stop();
+        if (driver != null) {
+            driver.quit();
+        }
+
+        if (browserStackLocal != null) {
+            browserStackLocal.stop();
+        }
     }
 }
---
+++ src/test/java/com/browserstack/Parallelized.java
@@ -0,0 +1,39 @@
+package com.browserstack;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.model.RunnerScheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Parallelized extends Parameterized {
+
+    public Parallelized(Class<?> klass) throws Throwable {
+        super(klass);
+        setScheduler(new ThreadPoolScheduler());
+    }
+
+    private static class ThreadPoolScheduler implements RunnerScheduler {
+        private ExecutorService executor;
+
+        public ThreadPoolScheduler() {
+            String threads = System.getProperty("junit.parallel.threads", "16");
+            int numThreads = Integer.parseInt(threads);
+            executor = Executors.newFixedThreadPool(numThreads);
+        }
+
+        public void finished() {
+            executor.shutdown();
+            try {
+                executor.awaitTermination(10 * 60, TimeUnit.SECONDS);
+            } catch (InterruptedException exc) {
+                throw new RuntimeException(exc);
+            }
+        }
+
+        public void schedule(Runnable childStatement) {
+            executor.submit(childStatement);
+        }
+    }
+}
